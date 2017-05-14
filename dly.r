e68d15ae22f689227fbb45a8e148982826f1c7a9
mzh <ma.hed@bluewin.ch>
2017-05-14 09:45:24 +0200

    Adjusting image

--- dly.r
+++ dly.r
@@ -29,16 +29,27 @@ d <- d[d$halt_kurz_von1 %in% stationen_linie_10]
 
 d$versp <- d$ist_an_von - d$soll_an_von
 
+# Omit too large delays.
+d <- d[abs(d$versp)<3000]
+
+
+
 library(ggplot2)
+
+# Font size of axis adjusted to png file output.
+# png("./014-vbz.png", width=1000, height=1500)
+# print(g)
+# dev.off()
 
 g <- ggplot(d, aes(betriebsdatum, versp))
 g <- g + geom_line() + facet_wrap(~halt_kurz_von1)
-g <- g + theme(axis.text.x = element_text(angle=45, size=18),
+g <- g + theme(axis.text.x = element_text(angle=90, size=24),
                axis.title.x = element_text(size=24),
                axis.title.y = element_text(size=24),
-               axis.text.y = element_text(size=20))
-g <- g + theme(strip.text.x = element_text(size=18))
+               axis.text.y = element_text(size=24))
+g <- g + theme(strip.text.x = element_text(size=24))
 g <- g + xlab("Date of Operation")
 g <- g + ylab("Total Delay/d [s]")
 g <- g + scale_x_date(date_breaks="5 months")
-# print(g)
+
+
